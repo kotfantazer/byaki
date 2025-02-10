70d7f9f319233777f4f2262cf69f16698e77f2fb
Nikita Gorshkov <kotfantazer@gmail.com>
2025-02-10 23:27:59 +0300

    попытка написать скрипты

--- build-map.ps1
+++ build-map.ps1
@@ -1,1 +1,3 @@
-"C:\Program Files\Therion\therion.exe" -x _map\_th\p\p_work.thcfg _export\byaki_map.pdf
+$curpath = (Get-Item .).FullName
+Set-Location $curpath
+& "C:\Program Files\Therion\therion.exe" -x "${curpath}\_map\_th\p\p_work.thcfg" "${curpath}\_export\byaki_map.pdf"
