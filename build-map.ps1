a4291e310d2b080457ca7e7ba10edadc54ebd684
Nikita Gorshkov <kotfantazer@gmail.com>
2025-02-11 15:55:39 +0300

    починил скрипт

--- build-map.ps1
+++ build-map.ps1
@@ -1,5 +1,3 @@
 $curpath = (Get-Item .).FullName
-Set-Location $curpath
-"${curpath}\_map\_th\p\p_work.thcfg"
-"${curpath}\_export\byaki_map.pdf"
-& "C:\Program Files\Therion\therion.exe" -x "${curpath}\_map\_th\p\p_work.thcfg" "${curpath}\_export\byaki_map.pdf"
+Set-Location "${curpath}\_map\_th\p\"
+& "C:\Program Files\Therion\therion.exe" -x "${curpath}\_map\_th\p\p_work.thcfg"
